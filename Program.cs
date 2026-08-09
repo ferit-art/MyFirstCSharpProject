f442e0931ba66c59536fb15d693a518c7e273070
Ferit Bat <feritbat6@gmail.com>
2026-08-09 23:15:08 +0200

    1. Day

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TodoApp
+﻿using MySqlConnector;
+
+namespace TodoApp
 {
     class Program
     {
@@ -11,10 +13,11 @@
                 Console.WriteLine("\n" + "=== TODO-LIST ===");
                 Console.WriteLine("1. Show all tasks");
                 Console.WriteLine("2. Add new task");
-                Console.WriteLine("3. Exit");
+                Console.WriteLine("3. Complete task");
+                Console.WriteLine("4. Exit");
                 Console.WriteLine("Choose between alternatives (1-3): ");
 
-                string choice = Console.ReadLine();
+                string choice = Console.ReadLine() ?? "";
 
                 switch (choice)
                 {
@@ -28,7 +31,14 @@
                         AddTask(title);
                         break;
 
+
                     case "3":
+                        Console.WriteLine("\n" + "The task's title: ");
+                        string completed_title = Console.ReadLine();
+                        CompleteTask(completed_title);
+                        break;
+
+                    case "4":
                         Console.WriteLine("\n" + "Bye!");
                         return;
 
@@ -41,11 +51,73 @@
 
         public static void ShowTasks()
         {
+            using var connection = new MySqlConnection(connectionString);
 
+            try
+            {
+                connection.Open();
+                string sql = "SELECT * FROM todos";
+                using var command = new MySqlCommand(sql, connection);
+                using var reader = command.ExecuteReader();
+
+                Console.WriteLine("\n" + "=== YOUR TODO LIST ===");
+                Console.WriteLine("---------------------------------------------");
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32("id");
+                    string title = reader.GetString("title");
+                    bool isCompleted = reader.GetBoolean("is_completed");
+
+                    string status = isCompleted ? "[DONE]" : "[]";
+                    Console.WriteLine($"{id}. {status} {title}");
+                }
+                Console.WriteLine("---------------------------------------------");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + $"Database Error: {e.Message}");
+            }
         }
 
-        public static void AddTask(string title) {
+        public static void AddTask(string title)
+        {
+            using var connection = new MySqlConnection(connectionString);
 
+            try
+            {
+                connection.Open();
+                string sql = "INSERT INTO todos (title) Values (@title)";
+                using var command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@title", title);
+                command.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + $"Database Error: {e.Message}");
+            }
+
+            ShowTasks();
+        }
+
+        public static void CompleteTask(string title)
+        {
+            using var connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                string sql = "UPDATE `todos` SET `is_completed`='1' WHERE title LIKE (@title)";
+                using var command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@title", title);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + $"Database Error: {e.Message}");
+            }
+            ShowTasks();
         }
     }
 }
